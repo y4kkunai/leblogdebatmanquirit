7b95f4bb8d0646647015c5419f0c0fd9b7ad2974
Axel <aa@aa.aa>
2023-06-07 17:02:09 +0200

    Ajout de page de profil

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,19 +2,35 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use         Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class MainController extends AbstractController
 {
 
     /**
-    *Contrôleur de la page d'accueil
-    */
+     * Contrôleur de la page d'accueil
+     */
     #[Route('/', name: 'main_home')]
     public function home(): Response
     {
         return $this->render('main/home.html.twig');
-}
+    }
+
+    /**
+     * Contrôleur de la page de profil
+     *
+     * Accès réservé aux personnes connectées (ROLE_USER)
+     */
+
+    #[Route('/mon-profil/', name: 'main_profil')]
+    #[IsGranted('ROLE_USER')]
+    public function profil(): Response
+    {
+        return $this->render('main/profil.html.twig');
+
+
+    }
 }
