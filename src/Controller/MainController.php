9c6b578835770a5c41309e08206e5c102a90b380
Axel <aa@aa.aa>
2023-06-07 10:01:40 +0200

    Mise en place du squelette front + ajout page d'accueil

---
+++ src/Controller/MainController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+
+    /**
+    *Contrôleur de la page d'accueil
+    */
+    #[Route('/', name: 'main_home')]
+    public function home(): Response
+    {
+        return $this->render('main/home.html.twig');
+}
+}
